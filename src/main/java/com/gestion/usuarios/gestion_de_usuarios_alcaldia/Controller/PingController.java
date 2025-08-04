8cb3d937e8db7dc0613c0967471b939d546a2207
Yojhan Arias Correa <125290541+YojhanxA@users.noreply.github.com>
2025-08-04 15:49:56 -0500

    Controlador para el ping

---
+++ src/main/java/com/gestion/usuarios/gestion_de_usuarios_alcaldia/Controller/PingController.java
@@ -0,0 +1,13 @@
+package com.gestion.usuarios.gestion_de_usuarios_alcaldia.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Backend activo";
+    }
+}
