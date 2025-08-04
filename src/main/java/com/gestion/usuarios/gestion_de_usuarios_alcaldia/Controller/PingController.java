51c723f95ede6c1ea9a1814aca4a7858be0aac05
Yojhan Arias Correa <125290541+YojhanxA@users.noreply.github.com>
2025-08-04 15:50:35 -0500

    Update PingController.java

--- src/main/java/com/gestion/usuarios/gestion_de_usuarios_alcaldia/Controller/PingController.java
+++ src/main/java/com/gestion/usuarios/gestion_de_usuarios_alcaldia/Controller/PingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingController {
 
-    @GetMapping("/ping")
+    @GetMapping("/")
     public String ping() {
         return "Backend activo";
     }
